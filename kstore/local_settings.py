814a28936f13e197b6c7c8bd5d88de5136dbd760
Anand Chitipothu <anandology@gmail.com>
2011-11-24 07:59:10 +0530

    Keeping the db and log files ourside the code.

--- kstore/local_settings.py
+++ kstore/local_settings.py
@@ -11,6 +11,7 @@ if LOCAL_DEV:
     INTERNAL_IPS = ('127.0.0.1',)
 
 DIRNAME = os.path.dirname(os.path.abspath(__file__))
+REPOROOT = os.path.dirname(DIRNAME)
 
 SATCHMO_DIRNAME = DIRNAME
     
@@ -28,7 +29,7 @@ DATABASES = {
     'default': {
         # The last part of ENGINE is 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'ado_mssql'.
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(DIRNAME, 'simple.db'),  # Or path to database file if using sqlite3
+        'NAME': os.path.join(REPOROOT, 'kstore.db'),  # Or path to database file if using sqlite3
         #'USER': '',             # Not used with sqlite3.
         #'PASSWORD': '',         # Not used with sqlite3.
         'HOST': '',             # Set to empty string for localhost. Not used with sqlite3.
@@ -62,7 +63,7 @@ LOGFILE = "satchmo.log"
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%a, %d %b %Y %H:%M:%S',
-                    filename=os.path.join(DIRNAME,LOGFILE),
+                    filename=os.path.join(REPOROOT,LOGFILE),
                     filemode='w')
 
 logging.getLogger('keyedcache').setLevel(logging.INFO)
