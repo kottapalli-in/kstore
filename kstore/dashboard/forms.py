88ddeea13b440714b70c4b3d667985e6f191696d
ashok <ashok.raavi@whitekites.com>
2011-12-09 23:57:28 +0530

    Dashboard formset

---
+++ kstore/dashboard/forms.py
@@ -0,0 +1,67 @@
+
+from django import forms
+from django.contrib.sites.models import Site
+from django.template.defaultfilters import slugify
+
+from product.models import AttributeOption, Category, Price, Product, ProductAttribute, ProductImage
+
+# any form field not in this list is considered as product attribute
+NOT_PRODUCT_ATTRS = ['title', 'price', 'category', 'cover_image']
+
+class ProductForm(forms.Form):
+    title = forms.CharField()
+    author = forms.CharField(required=False)
+    publisher = forms.CharField(required=False)
+    published_date = forms.CharField(required=False)
+    isbn = forms.CharField(required=False)
+    price = forms.DecimalField()
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), required=False)
+    cover_image = forms.ImageField(required=False)
+
+    def save(self):
+        data = self.cleaned_data
+        site = Site.objects.get_current()
+
+        # create product
+        product = Product(name=data['title'], slug=slugify(data['title']), site=site, active=True)
+        product.save()
+
+        # create product price
+        price = Price(price=data['price'], product=product)
+        price.save()
+
+        if data.get('category'):
+            # get or create category
+            category, created = Category.objects.get_or_create(site=site, name=data['category'])
+            # TODO: remove existing categories of the product
+            product.category.add(category)
+
+        if data.get('cover_image'):
+            # create product image
+            ProductImage.objects.create(product=product, picture=data['cover_image'])
+
+        # check for product attributes 
+        for key in data:
+            # if key is not product attribute or
+            # value for the key is blank, goto next key
+            if key in NOT_PRODUCT_ATTRS or not data[key]:
+                continue
+
+            # get the field related to the key
+            field = self.fields[key]
+            description = (field.label or key.replace('_', ' ')).capitalize()
+
+            # get or create the attribute
+            try:
+                attribute = AttributeOption.objects.get(name=key)
+            except AttributeOption.DoesNotExist:
+                attribute = AttributeOption(name=key,
+                                            description=description,
+                                            validation='product.utils.validation_simple')
+                attribute.save()
+
+            # create product attribute
+            product_attribute = ProductAttribute(product=product,
+                                                 option=attribute,
+                                                 value=data[key])
+            product_attribute.save()
--- kstore/dashboard/views.py
+++ kstore/dashboard/views.py
@@ -1,8 +1,9 @@
-from django.contrib.sites.models import Site
+
+from django.forms.formsets import formset_factory
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
-from django.template.defaultfilters import slugify
 
+from dashboard.forms import ProductForm
 from product.models import Product, Price
 
 
@@ -12,22 +13,16 @@ def index(request):
 
 
 def addbooks(request):
-    ctx = RequestContext(request, {})
-    site = Site.objects.get_current()
+    ProductFormSet = formset_factory(ProductForm, extra=9)
+
     if request.method == 'POST':
-        post_data = request.POST
-        data = zip(post_data.getlist('title'),
-                    post_data.getlist('author'),
-                    post_data.getlist('publisher'),
-                    post_data.getlist('published_date'),
-                    post_data.getlist('isbn'),
-                    post_data.getlist('price'))
-        data = [item for item in data if item[0].strip()]
-        import pdb;pdb.set_trace()
-        for data in data:
-            product = Product(name=data[0], slug=slugify(data[0]), site=site, active=True)
-            product.save()
-            price = Price(price=data[5], product=product)
-            price.save()
-        redirect('dashboard_addbooks')
+        formset = ProductFormSet(request.POST, request.FILES)
+        if formset.is_valid():
+            for form in formset:
+                if form.cleaned_data:
+                    form.save()
+        return redirect('dashboard_addbooks')
+    else:
+        formset = ProductFormSet()
+    ctx = RequestContext(request, {'formset': formset})
     return render_to_response('dashboard/addbooks.html', context_instance=ctx)
