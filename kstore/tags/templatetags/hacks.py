967574be597af205662b7271e4eb2ed1c1e17930
Anand Chitipothu <anandology@gmail.com>
2011-12-21 09:48:31 +0530

    fix.

--- kstore/tags/templatetags/hacks.py
+++ kstore/tags/templatetags/hacks.py
@@ -1,8 +1,11 @@
 from django import template
+import itertools
 
 register = template.Library()
 
 @register.filter
 def product_attrs(product):
-    attrs = product.translated_attributes('te') + product.translated_attributes()
+    attrs = itertools.chain(
+        product.translated_attributes('te'),
+        product.translated_attributes())
     return dict((a.name, a.value) for a in attrs)
